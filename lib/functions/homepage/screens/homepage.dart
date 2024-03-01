fe0f66c51d0615515e253a9619c765ccc074e361
vaishnav <mysecondmailspam@gmail.com>
2024-03-01 14:41:28 +0530

    Changed the api to openlibrary

--- lib/functions/homepage/screens/homepage.dart
+++ lib/functions/homepage/screens/homepage.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+//import 'package:csv/csv.dart';
+//import 'package:flutter/services.dart' show rootBundle;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -11,67 +15,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
 
+  // void exportData() async {
+  //   final CollectionReference library =
+  //       FirebaseFirestore.instance.collection("library1");
+  //   final myData = await rootBundle.loadString('assets/booksdata/books.csv');
+  //   List<List<dynamic>> csvTable = CsvToListConverter().convert(myData);
+  //   List<List<dynamic>> data = [];
+  //   data = csvTable;
+  //   for (var i = 0; i < data.length; i++) {
+  //     var record = {
+  //       "Title": data[i][0].toString(), // Cast to string explicitly
+  //       "Author": data[i][1].toString(), // Cast to string explicitly
+  //       "Genre": data[i][2].toString() // Cast to string explicitly
+  //     };
+  //     library.add(record);
+  //   }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Firestore JSON Upload'),
+        title: const Text('HomePage'),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(5),
-          child: ListView(
-            children: [
-              const Text(
-                'Search for a book',
-                style: TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-                textAlign: TextAlign.start,
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    for (int i = 1; i <= 4; i++)
-                      FutureBuilder<DocumentSnapshot>(
-                        future: FirebaseFirestore.instance
-                            .collection('library1')
-                            .doc('book$i')
-                            .get(),
-                        builder: (context, snapshot) {
-                          if (snapshot.connectionState == ConnectionState.waiting) {
-                            return CircularProgressIndicator();
-                          } else if (snapshot.hasError) {
-                            return Text('Error: ${snapshot.error}');
-                          } else {
-                            var imageUrl = snapshot.data!['coverUrl'];
-                            return Padding(
-                              padding: const EdgeInsets.symmetric(horizontal:8.0,vertical:18.0),
-                              child: Image.network(
-                                imageUrl,
-                                height: 300,
-                                width: 150,
-                                fit: BoxFit.contain,
-                              ),
-                            );
-                          }
-                        },
-                      ),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 5),
-              // Bottom navigation bar
-            ],
-          ),
+        child: ListView(
+          children: [
+            // ElevatedButton(onPressed:() => exportData(), child: Text('store csv')),
+            StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('library1').snapshots(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return CircularProgressIndicator();
+                }
+                final books = snapshot.data!.docs;
+                List<String> bookTitles = [];
+                for (var book in books) {
+                  bookTitles.add(book['Title'].toString());
+                }
+                return SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: _buildBookRows(bookTitles),
+                  ),
+                );
+              },
+            ),
+          ],
         ),
       ),
-      backgroundColor: Color.fromARGB(255, 2, 14, 39),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentIndex,
@@ -95,5 +87,71 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  List<Widget> _buildBookRows(List<String> bookTitles) {
+    List<Widget> rows = [];
+    for (var i = 0; i < bookTitles.length; i += 5) {
+      List<String> currentRow = bookTitles.sublist(
+          i, i + 5 > bookTitles.length ? bookTitles.length : i + 5);
+      rows.add(_buildBookRow(currentRow));
+    }
+    return rows;
+  }
+
+  Widget _buildBookRow(List<String> bookTitles) {
+    return ListView(
+      scrollDirection: Axis.horizontal,
+      children: [
+        for (var bookTitle in bookTitles)
+          FutureBuilder<dynamic>(
+            future: fetchBookData(bookTitle),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return SizedBox(
+                  width: 150,
+                  height: 300,
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else {
+                var imageUrl = snapshot.data!['coverUrl'];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    width: 150,
+                    height: 300,
+                    child: Image.network(
+                      imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
+              }
+            },
+          ),
+      ],
+    );
+  }
+
+
+  Future<dynamic> fetchBookData(String bookTitle) async {
+    var url = 'http://openlibrary.org/search.json?title=$bookTitle';
+
+    var response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      var docs = data['docs'];
+      if (docs != null && docs.isNotEmpty) {
+        var coverId = docs[0]['cover_i'];
+        var coverUrl = 'https://covers.openlibrary.org/b/id/$coverId-L.jpg';
+        return {'coverUrl': coverUrl};
+      }
+    }
+    // Return a default cover URL if no data is found
+    return {'coverUrl': 'https://via.placeholder.com/150'};
   }
 }
