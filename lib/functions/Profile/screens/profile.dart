faf082c9e12575ff09a3b63f08f0958c72334aa9
vaishnav <98941142+Vaishnavap@users.noreply.github.com>
2024-05-14 11:40:54 +0530

    added profile

--- lib/functions/Profile/screens/profile.dart
+++ lib/functions/Profile/screens/profile.dart
@@ -1,9 +1,22 @@
-import 'package:bookwise/functions/loginandsignup/screens/login.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:bookwise/functions/loginandsignup/screens/login.dart';
 
-class MyProfile extends StatelessWidget {
-  const MyProfile({Key? key}) : super(key: key);
+class MyProfile extends StatefulWidget {
+  final String uid;
+  const MyProfile({Key? key, required this.uid}) : super(key: key);
+
+  @override
+  State<MyProfile> createState() => _MyProfileState();
+}
+
+class _MyProfileState extends State<MyProfile> {
+  var userDetails = {};
+  int followers = 0;
+  int following = 0;
+  bool isFollowing = false;
+  bool isLoading = false;
 
   Future<void> _logout(BuildContext context) async {
     try {
@@ -11,8 +24,8 @@ class MyProfile extends StatelessWidget {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-            builder: (context) =>
-                const LoginPage()), // Navigate to the login screen after logout
+          builder: (context) => const LoginPage(),
+        ),
       );
     } catch (e) {
       print('Error logging out: $e');
@@ -21,234 +34,225 @@ class MyProfile extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
-    User? user = FirebaseAuth.instance.currentUser;
-    String userName = user?.displayName ?? user?.displayName ?? user?.email ?? 'Guest';
+  void initState() {
+    super.initState();
+    _loadUserData();
+  }
 
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        title: const Text(
-          'Profile',
-          style: TextStyle(color: Colors.black87),
-        ),
-        leading: Builder(
-          builder: (context) => PopupMenuButton(
-            icon: Icon(Icons.menu, color: Colors.black87),
-            itemBuilder: (context) => [
-              PopupMenuItem(
-                child: ListTile(
-                  title: Text('Logout'),
-                  leading: Icon(Icons.exit_to_app),
-                  onTap: () {
-                    _logout(context);
-                    // Call logout function here
-                  },
-                ),
+  @override
+  void didUpdateWidget(covariant MyProfile oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.uid != widget.uid) {
+      _loadUserData();
+    }
+  }
+
+  Future<void> _loadUserData() async {
+    setState(() {
+      isLoading = true;
+    });
+    try {
+      var snap = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(widget.uid)
+          .get();
+
+      if (snap.data() != null) {
+        userDetails = snap.data()!;
+        followers = snap.data()!['followers'].length;
+        following = snap.data()!['following'].length;
+
+        // Update isFollowing state based on fetched followers list
+       isFollowing = snap.data()!['followers'] == null
+          ? false
+          : snap.data()!['followers'].contains(FirebaseAuth.instance.currentUser!.uid);
+
+      }
+    } catch (e) {
+      print('Error in loading user data: $e');
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              elevation: 0,
+              title: const Text(
+                'Profile',
+                style: TextStyle(color: Colors.black87),
               ),
-            ],
-          ),
-        ),
-        backgroundColor: Colors.white,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Row(
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    // backgroundImage: AssetImage('49457.jpg'),
-                  )
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          child: Text(
-                            userName,
-                            style: TextStyle(
-                                color: Colors.grey[800],
-                                fontSize: 10,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Container(
-                          child: Text(
-                            '@vaiz',
-                            style: TextStyle(
-                                color: Colors.blueGrey[400],
-                                fontSize: 8,
-                                fontWeight: FontWeight.w600),
-                          ),
-                        ),
-                      ],
+              leading: Builder(
+                builder: (context) => PopupMenuButton(
+                  icon: Icon(Icons.menu, color: Colors.black87),
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: ListTile(
+                        title: Text('Logout'),
+                        leading: Icon(Icons.exit_to_app),
+                        onTap: () {
+                          _logout(context);
+                        },
+                      ),
                     ),
-                    Row(
-                      children: [
-                        Container(
-                          height: 40,
-                          width: 40,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border:
-                                Border.all(width: 1, color: Colors.blueGrey),
-                          ),
-                          child: Center(
-                              child: Icon(
-                            Icons.message,
-                            color: Colors.blueGrey[400],
-                          )),
-                        ),
-                        Container(
-                          width: 70,
-                          height: 40,
-                          padding: const EdgeInsets.only(left: 20),
-                          child: TextButton(
-                            style: ButtonStyle(
-                              shape: MaterialStateProperty.all<
-                                  RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                  side: const BorderSide(
-                                    color: Colors.blue,
-                                    width: 1,
-                                    style: BorderStyle.solid,
-                                  ),
-                                  borderRadius: BorderRadius.circular(50),
-                                ),
-                              ),
-                              backgroundColor:
-                                  MaterialStateProperty.resolveWith<Color>(
-                                (Set<MaterialState> states) {
-                                  if (states.contains(MaterialState.disabled)) {
-                                    return Colors.blue; // Disabled color
-                                  }
-                                  return Colors.blue; // Normal color
-                                },
-                              ),
-                            ),
-                            onPressed: () {
-                              // Add onPressed functionality here
-                            },
-                            child: const Text(
-                              'Follow',
-                              style: TextStyle(color: Colors.white,fontSize:8 ),
-                              
-                            ),
-                          ),
-                        ),
-                      ],
-                    )
                   ],
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                child: Divider(
-                  thickness: 1,
-                  color: Colors.blueGrey[200],
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    padding:const  EdgeInsets.only(top: 30),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
+              ],
+              backgroundColor: Colors.white,
+            ),
+            body: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
                       children: [
-                        Container(
-                          child: const Text(
-                            '203',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800, fontSize: 20),
-                          ),
+                        CircleAvatar(
+                          radius: 40,
+                          // backgroundImage: AssetImage('49457.jpg'),
                         ),
                         Container(
-                          child:const  Text(
-                            'Followers',
-                            style:
-                                TextStyle(fontSize: 15, color: Colors.blueGrey),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.only(top: 30),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          child: const Text(
-                            '932',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800, fontSize: 20),
-                          ),
-                        ),
-                        Container(
-                          child: const Text(
-                            'Following',
-                            style:
-                                TextStyle(fontSize: 15, color: Colors.blueGrey),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding:const  EdgeInsets.only(top: 30),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          child:const  Text(
-                            '30',
-                            style: TextStyle(
-                                fontWeight: FontWeight.w800, fontSize: 20),
+                          padding: EdgeInsets.only(left: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                userDetails['username'],
+                                style: TextStyle(
+                                  color: Color.fromARGB(255, 1, 29, 46),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '@vaiz',
+                                style: TextStyle(
+                                  color: Colors.blueGrey[400],
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        Container(
-                          child:const  Text(
-                            'Projects',
-                            style:
-                                TextStyle(fontSize: 15, color: Colors.blueGrey),
-                          ),
-                        )
                       ],
                     ),
-                  ),
-                ],
-              ),
-              Container(
-                padding:const  EdgeInsets.only(top: 20),
-                child: Divider(
-                  thickness: 1,
-                  color: Colors.blueGrey[200],
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          '$followers Followers',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 13, 134, 204),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        SizedBox(width: 20),
+                        Text(
+                          '$following Following',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 13, 134, 204),
+                            fontSize: 15,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                     onPressed: () async {
+  if (FirebaseAuth.instance.currentUser!.uid == widget.uid) {
+    setState(() {
+      following = followers; // If viewing own profile, set following count equal to followers count
+    });
+    return;
+  }
+
+  setState(() {
+    isFollowing = !isFollowing; // Toggle follow/unfollow status
+    followers += isFollowing ? 1 : -1; // Increment/decrement followers count based on follow/unfollow
+  });
+
+  // Update followers list for the user being followed
+  await FirebaseFirestore.instance
+    .collection('users')
+    .doc(widget.uid)
+    .update({
+      'followers': isFollowing
+          ? FieldValue.arrayUnion([FirebaseAuth.instance.currentUser!.uid])
+          : FieldValue.arrayRemove([FirebaseAuth.instance.currentUser!.uid]),
+    });
+
+  // Update following list for the current user
+  await FirebaseFirestore.instance
+    .collection('users')
+    .doc(FirebaseAuth.instance.currentUser!.uid)
+    .update({
+      'following': isFollowing
+          ? FieldValue.arrayUnion([widget.uid])
+          : FieldValue.arrayRemove([widget.uid]),
+    });
+},
+
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(30.0), // Adjust as needed
+                        ),
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 16.0, vertical: 12.0),
+                        backgroundColor:
+                            FirebaseAuth.instance.currentUser!.uid == widget.uid
+                                ? Colors.grey // Disabled state color (optional)
+                                : (isFollowing
+                                    ? Colors.blueAccent
+                                    : Color.fromARGB(255, 13, 134,
+                                        204)), // Following/Unfollow colors
+                        foregroundColor: Colors.white, // Text color
+                        textStyle: TextStyle(
+                            fontSize: 16.0), // Text style customization
+                      ),
+                      child: Row(
+                        mainAxisSize:
+                            MainAxisSize.min, // Center content horizontally
+                        children: [
+                          FirebaseAuth.instance.currentUser!.uid == widget.uid
+                              ? Icon(Icons.person_add,
+                                  size: 18.0) // "Add friend" icon
+                              : (isFollowing
+                                  ? Icon(Icons.check, size: 18.0)
+                                  : Icon(Icons.add, size: 18.0)),
+                          SizedBox(width: 8.0), // Spacing between icon and text
+                          Text(
+                            FirebaseAuth.instance.currentUser!.uid == widget.uid
+                                ? 'Add Friend' // "Add friend" text
+                                : (isFollowing ? 'Following' : 'Follow'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.only(top: 20),
-                child: Divider(
-                  thickness: 1,
-                  color: Colors.blueGrey[200],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/functions/loginandsignup/screens/signup.dart
+++ lib/functions/loginandsignup/screens/signup.dart
@@ -1,7 +1,9 @@
 import 'package:bookwise/common/toast.dart';
+import 'package:bookwise/functions/homepage/screens/homepage.dart';
 import 'package:bookwise/functions/loginandsignup/firebase_auth_ser.dart';
 import 'package:bookwise/functions/loginandsignup/screens/login.dart';
 import 'package:bookwise/widgets/form_container_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -15,9 +17,9 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   final FirebaseAuthService _auth = FirebaseAuthService();
 
-  TextEditingController _usernameController = TextEditingController();
-  TextEditingController _emailController = TextEditingController();
-  TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   bool isSigningUp = false;
 
@@ -143,10 +145,28 @@ class _SignUpState extends State<SignUp> {
       isSigningUp = false;
     });
     if (user != null) {
+      await addUserDetails(username, email, user.uid);
       showToast(message: "User is successfully created");
-      Navigator.pushReplacementNamed(context, "/home");
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => HomePage()));
     } else {
       showToast(message: "Some error happened");
     }
   }
 }
+
+Future<void> addUserDetails(String username, String email, String uid) async {
+  try {
+    await FirebaseFirestore.instance.collection('users').doc(uid).set({
+      'username': username,
+      'email': email,
+      'uid': uid,
+      'followers': [],
+      'following': [],
+    });
+  } catch (e) {
+    print('Error adding user details: $e');
+    throw e; // Rethrow the error to handle it where addUserDetails is called
+  }
+}
+
